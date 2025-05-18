d36e11b436e2042a565d788bcc83d39246aa39b9
XuchenMa <xuchenma@stu.ecnu.edu.cn>
2025-05-18 17:25:18 +0800

    feature..

--- feature.py
+++ feature.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.nn.functional import one_hot
 from load_datas import OurData, Dataloader
 
 device = "cuda:2"
@@ -67,12 +68,16 @@ def get_features(data: OurData, print_size=False):
     category_size = 7
     sub_category_size = 29
     history_dim = w_num * category_size
-    p_dim = 3 * 661
+    max_entry_num = 661
+
+
+    p_dim = 2 * max_entry_num
     # workers_quality = (torch.tensor([wws[w_index2id[i]] for i in range(w_num)])) / 100
     workers_history = [[0. for i in range(category_size)] for j in range(w_num)]
-    workers_history_count = [[-1. for i in range(category_size)] for j in range(w_num)]
+    workers_history_count = [[-1 for i in range(category_size)] for j in range(w_num)]
     # quality score withdrawn
-    p_history = [[0. for i in range(p_dim)] for j in range(p_num)]
+    p_history = [torch.zeros(p_dim) for j in range(p_num)]
+    p_history_count = [0 for j in range(p_num)]
     
     last_r_state, last_w_state = None, None
     for k, e in enumerate(es):
@@ -81,48 +86,56 @@ def get_features(data: OurData, print_size=False):
         p_id = e['project_id']
         if not w_id in w_id2index:
             continue
-        state, action, reward = torch.tensor(p_history[p_id2index[str(p_id)]], dtype=torch.float16), torch.tensor([wws[w_id]], dtype=torch.float16), e["withdrawn"] * e['score']
         p = ps[str(p_id)]
+        category_feature = one_hot(torch.tensor(p['category']), category_size)
+        p_feature = torch.tensor(p_history[p_id2index[str(p_id)]], dtype=torch.float16)
+        state, action, reward = torch.cat((category_feature, p_feature), dim=0).float(), torch.tensor([wws[w_id]], dtype=torch.float16), e["withdrawn"] * e['score']
 
         action = torch.cat((action, torch.tensor(workers_history[w_id2index[w_id]], dtype=torch.float16) / \
                             torch.tensor(workers_history_count[w_id2index[w_id]], dtype=torch.float16)), dim=0)
 
         # 更新worker history
         workers_history[w_id2index[w_id]][p['category']] += reward
-        workers_history_count[w_id2index[w_id]][p['category']] += 1 if workers_history_count[w_id2index[w_id]] != -1 else 2
+        workers_history_count[w_id2index[w_id]][p['category']] += 1 if workers_history_count[w_id2index[w_id]][p['category']] != -1 else 2
+
+        # 更新p history
+        idx = p_history_count[p_id2index[str(p_id)]] * 2
+
+        p_history[p_id2index[str(p_id)]][idx] += e["withdrawn"] * e['score']
+        p_history[p_id2index[str(p_id)]][idx+1] += wws[w_id]
+        p_history_count[p_id2index[str(p_id)]] += 1
         
         # s1状态就是上一个s2状态
         if last_r_state is None:
             last_r_state = state
-            last_w_state = action
             continue
 
         requester_data['s1'].append(last_r_state)
-        worker_data['s1'].append(last_w_state)
         requester_data['s2'].append(state)
-        worker_data['s2'].append(action)
 
         last_r_state = state.clone()
-        last_w_state = action.clone()
 
         requester_data['a'].append(action.clone())
         requester_data['r'].append(reward)
 
-        worker_data['a'].append(state.clone())
         worker_data['r'].append(e["withdrawn"] * p['total_awards'] / p['entry_count'])
 
     requester_data['s1'] = torch.stack(requester_data['s1'], dim=0)
     requester_data['s2'] = torch.stack(requester_data['s2'], dim=0)
     requester_data['a'] = torch.stack(requester_data['a'], dim=0)
-    worker_data['s1'] = torch.stack(worker_data['s1'], dim=0)
-    worker_data['s2'] = torch.stack(worker_data['s2'], dim=0)
-    worker_data['a'] = torch.stack(worker_data['a'], dim=0)
+    # worker_data['s1'] = torch.stack(worker_data['s1'], dim=0)
+    # worker_data['s2'] = torch.stack(worker_data['s2'], dim=0)
+    # worker_data['a'] = torch.stack(worker_data['a'], dim=0)
+
+    requester_data['s1'] = (requester_data['s1'] - requester_data['s1'].mean(dim=0)) / (requester_data['s1'].std(dim=0) + 1e-6)
+    requester_data['s2'] = (requester_data['s2'] - requester_data['s2'].mean(dim=0)) / (requester_data['s2'].std(dim=0) + 1e-6)
+    requester_data['a'] = (requester_data['a'] - requester_data['a'].mean(dim=0)) / (requester_data['a'].std(dim=0) + 1e-6)
     print()
 
     if print_size:
         for k in ['s1', 's2', 'a']:
             print(("requester %s size: "%k) + str(requester_data[k].size()))
-            print(("worker %s size: "%k) + str(worker_data[k].size()))
+            # print(("worker %s size: "%k) + str(worker_data[k].size()))
 
 
     return requester_data, worker_data
